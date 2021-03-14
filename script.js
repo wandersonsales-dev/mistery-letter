03a92b7c5952c5d586d70e8c81946ae50014aa25
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-14 17:00:28 -0300

    :rocket: [Req. 05] - Mensagem de erro se mensagem vazia

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const showResult = document.querySelector('#carta-gerada');
 
 const generate = () => {
   showResult.innerHTML = '';
+  // https://pt.stackoverflow.com/questions/58498/se-input-tiver-apenas-espa%C3%A7os-em-branco-n%C3%A3o-fazer-nada
+  // Achei a função .trim() no link acima
+  if (input.value === '' || !input.value.trim()) {
+    showResult.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    return;
+  }
   const inputSplited = input.value.split(' ');
   for (let index = 0; index < inputSplited.length; index += 1) {
     const newSpan = document.createElement('span');
