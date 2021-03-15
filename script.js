8688cc041acfa0d2e84758285af499c79cf6ba76
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-15 18:15:43 -0300

    :rocket: [Req. 16] - Correção v3

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 const input = document.querySelector('#carta-texto');
 const buttonGenerate = document.querySelector('#criar-carta');
 const showResult = document.querySelector('#carta-gerada');
-const grupo1 = ['newspaper', 'magazine1', 'magazine2'];
-const grupo2 = ['medium', 'big', 'reallybig'];
-const grupo3 = ['rotateleft', 'rotateright'];
-const grupo4 = ['skewleft', 'skewright'];
-
-const group1Sorted = () => {
-  const number = Math.floor(Math.random() * (grupo1.length + 1));
-  if (grupo1[number] !== undefined) {
-    return grupo1[number];
-  }
+const classesObj = {
+  grupo1: ['newspaper', 'magazine1', 'magazine2'],
+  grupo2: ['medium', 'big', 'reallybig'],
+  grupo3: ['rotateleft', 'rotateright'],
+  grupo4: ['skewleft', 'skewright'],
 };
 
-const group2Sorted = () => {
-  const number = Math.floor(Math.random() * (grupo2.length + 1));
-  if (grupo2[number] !== undefined) {
-    return grupo2[number];
-  }
-};
-
-const group3Sorted = () => {
-  const number = Math.floor(Math.random() * (grupo3.length + 1));
-  if (grupo1[number] !== undefined) {
-    return grupo3[number];
-  }
-};
-
-const group4Sorted = () => {
-  const number = Math.floor(Math.random() * (grupo4.length + 1));
-  if (grupo1[number] !== undefined) {
-    return grupo4[number];
-  }
-};
-
-const generateClass = () => {
-  let newClass = '';
-  newClass += `${group1Sorted()} `;
-  newClass += `${group2Sorted()} `;
-  newClass += `${group3Sorted()} `;
-  newClass += `${group4Sorted()} `;
-  // https://stackoverflow.com/a/64448355
-  newClass = newClass.split('undefined').join('');
+const generateNewClass = () => {
+  const newClass = [];
+  Object.values(classesObj).forEach((el) => {
+    const random = Math.floor(Math.random() * (el.length + 1));
+    if (el.length !== random) {
+      newClass.push(el[random]);
+    }
+  });
   return newClass;
+};
+
+const checkClasses = (newClass) => {
+  const spans = document.querySelector('span');
+  let isEqual = false;
+  for (let index = 0; index < spans.length; index += 1) {
+    if (spans.className === newClass) {
+      isEqual = true;
+      break;
+    }
+  }
+  return isEqual;
+};
+
+const newClassRandom = (e) => {
+  do {
+    e.target.className = '';
+    generateNewClass().forEach((el) => {
+      e.target.className += `${el} `;
+    });
+  } while (checkClasses(e.target.className) === true);
+};
+
+const generateClass = (e) => {
+  if (e.className === '') {
+    console.log('1');
+    let newClass = '';
+    generateNewClass().forEach((el) => {
+      newClass += `${el} `;
+    });
+    return newClass;
+  }
+  newClassRandom(e);
 };
 
 const generate = () => {
@@ -57,8 +64,8 @@ const generate = () => {
   for (let index = 0; index < inputSplited.length; index += 1) {
     const newSpan = document.createElement('span');
     newSpan.innerHTML = inputSplited[index];
-    newSpan.className = generateClass();
-    newSpan.addEventListener('click', (e) => { e.target.className = generateClass(); });
+    newSpan.className = generateClass(newSpan);
+    newSpan.addEventListener('click', generateClass);
     showResult.appendChild(newSpan);
   }
 };
